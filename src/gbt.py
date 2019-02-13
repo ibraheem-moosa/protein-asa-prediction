23722042ce59ebe05727ad42276fc1238cd65f64
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2019-02-14 00:20:11 +0600

    GBT cross validation

--- src/gbt.py
+++ src/gbt.py
@@ -1,9 +1,15 @@
 from load_non_oh import *
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import cross_val_score
+import numpy as np
 import sys
 
 if __name__ == '__main__':
+
+    random.seed(42)
+    np.random.seed(42)
 
     if len(sys.argv) < 3:
         print('Usage: python _ data_dir ws')
@@ -12,6 +18,13 @@ if __name__ == '__main__':
     data_dir = sys.argv[1]
     ws = int(sys.argv[2])
     train_prot_windows, train_asas, test_prot_windows, test_asas = load_non_oh(data_dir, ws)
-    gbt = GradientBoostingRegressor(max_depth=3, n_estimators=1000, verbose=5, n_iter_no_change=5)
+    print('Dataset loaded')
+    gbt = GradientBoostingRegressor(max_depth=3, loss='lad', n_estimators=1000, verbose=5, n_iter_no_change=5)
+    print('Doing 5-fold cross validation')
+    cv_result = cross_val_score(gbt, train_prot_windows, train_asas, cv=5, scoring=make_scorer(mean_absolute_error), verbose=100)
+    print(cv_result)
+    print(cv_result.mean())
+
+    print('Calling fit')
     gbt.fit(train_prot_windows, train_asas)
     print(mean_absolute_error(test_asas, gbt.predict(test_prot_windows)))
--- src/rf.py
+++ src/rf.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     data_dir = sys.argv[1]
     ws = int(sys.argv[2])
     train_prot_windows, train_asas, test_prot_windows, test_asas = load_non_oh(data_dir, ws)
-    rf = RandomForestRegressor(max_depth=4, n_estimators=100)
+    rf = RandomForestRegressor(max_depth=4, n_estimators=100, verbose=10)
     rf.fit(train_prot_windows, train_asas)
     print(mean_absolute_error(test_asas, rf.predict(test_prot_windows)))
