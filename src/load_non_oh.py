5997d4f3449606dac608be07df0dd877fa15d5e9
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2019-02-19 23:45:50 +0600

    Some refactoring

--- src/load_non_oh.py
+++ src/load_non_oh.py
@@ -9,7 +9,7 @@ def load_non_oh(data_dir, ws, train_ratio=0.8):
     files = os.listdir(data_dir)
     for fname in files:
         protein, asa = parse_rsa(os.path.join(data_dir, fname))
-        protein = transform_input(protein, ws)
+        protein = windows_from_seq(protein, ws)
         proteins.append(protein)
         asas.append(asa)
     return split_train_test(proteins, asas, train_ratio)
